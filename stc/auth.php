3ec9ee791f7a9aef7a7f3f92b6664c8a80e51d2c
sunboy <qysk@vip.qq.com>
2024-12-17 11:13:36 +0800

    init

---
+++ stc/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    //登录页面
+    'auth_login' => 'index/login',
+    //超管账号 全权限 不做限制
+    'auth_admin' => 'admin',
+];
---
+++ stc/form.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 表单构建字段支持
+ */
+return [
+    'text' => '单行输入框',
+    'textarea' => '多行输入框',
+    'radio' => '单选框',
+    'checkbox' => '复选框',
+    'select' => '单选下拉框',
+    'image' => '单图上传',
+    'images' => '多图上传',
+    'file' => '单文件上传',
+    'files' => '多文件上传',
+    'date' => '日期',
+    'datetime' => '日期+时间',
+    'icon' => 'Icon图标',
+    'color' => '颜色选择器',
+    'editor' => '富文本编辑器(默认)',
+    'tinymce' => 'tinymce编辑器',
+    'wangeditor' => 'WangEditor',
+    'ueditor' => '百度Ueditor',
+    'usergroup' => '用户组单选',
+    'usergroup2' => '用户组多选',
+    'hidden' => '隐藏字段',
+];
