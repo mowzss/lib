cdf266dd30eeb872ac11fdb42cfa006a32260235
sunboy <qysk@vip.qq.com>
2024-12-27 15:59:46 +0800

    init

---
+++ src/forms/field/Editor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace mowzs\lib\forms\field;
+
+use mowzs\lib\forms\FormFieldRenderer;
+use mowzs\lib\forms\RendererInterface;
+
+class Editor extends FormFieldRenderer implements RendererInterface
+{
+    /**
+     * 渲染表单
+     * @param string $name
+     * @param string $label
+     * @param mixed $value
+     * @param mixed $option
+     * @param bool $required
+     * @param mixed $ext
+     * @return string
+     */
+    public function render(string $name, string $label, mixed $value, mixed $option, bool $required, mixed $ext): string
+    {
+        $required = $required ? 'required lay-verify="required"' : '';
+        return $this->fetch('editor', [
+            'name' => $name,
+            'label' => $label,
+            'value' => $value,
+            'required' => $required,
+            'ext' => $ext,
+        ]);
+    }
+}
