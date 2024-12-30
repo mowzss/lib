4f0284cbc9e78730ba3f18baf9bb7c383fbf4e43
sunboy <qysk@vip.qq.com>
2024-12-31 00:17:39 +0800

    init

---
+++ src/forms/field/Xmselect.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace mowzs\lib\forms\field;
+
+use mowzs\lib\forms\FormFieldRenderer;
+use mowzs\lib\forms\RendererInterface;
+
+class Xmselect extends FormFieldRenderer implements RendererInterface
+{
+    /**
+     * 渲染表单
+     * @param string $name
+     * @param string $label
+     * @param mixed $value
+     * @param mixed $option
+     * @param bool $required
+     * @param mixed $ext
+     * @return string
+     */
+    public function render(string $name, string $label, mixed $value, mixed $option, bool $required, mixed $ext): string
+    {
+        if (!is_array($option)) {
+            $option = explode('|', $option);
+        }
+        $required = $required ? 'required lay-verify="required"' : '';
+        return $this->fetch('xmselect', [
+            'name' => $name,
+            'label' => $label,
+            'value' => $value,
+            'required' => $required,
+            'option' => $option,
+            'ext' => $ext,
+        ]);
+    }
+}
