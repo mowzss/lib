15f3b7ded99af02fe39149eb1013e2ef37b6ae57
sunboy <qysk@vip.qq.com>
2025-03-06 23:02:46 +0800

    refactor(happy-system): 重构命令行接口和系统配置
    - 移除 commands.php 文件，将命令行接口配置移至 sys.php
    - 更新 composer.json，增加 common/command 和 common/task 的映射- 修改 Service.php，优化命令行接口的加载逻辑
    - 新增 sys.php 文件，重新配置命令行接口

--- src/Service.php
+++ src/Service.php
@@ -60,12 +60,6 @@ class Service extends BaseService
             TaskRun::class,
             TaskSchedule::class,
         ]);
-        if (is_file($this->app->getBasePath() . 'commands.php')) {
-            $command = include $this->app->getBasePath() . 'commands.php';
-            if (is_array($command)) {
-                $this->commands($command);
-            }
-        }
     }
 
     /**
@@ -74,10 +68,31 @@ class Service extends BaseService
      */
     protected function loadSysFiles(): void
     {
-        $baseDir = $this->app->getBasePath() . DIRECTORY_SEPARATOR;
+        $baseDir = $this->app->getAppPath() . DIRECTORY_SEPARATOR;
+        $paths = [
+            $baseDir . 'sys.php',
+            $baseDir,
+        ];
 
+        foreach ($paths as $path) {
+            if (is_dir($path)) {
+                // 如果是目录，则遍历子目录寻找 sys.php 文件
+                $this->scanDirectoryForSysFiles($path);
+            } elseif (is_file($path)) {
+                // 如果是文件，则直接加载
+                $this->includeSysFile($path);
+            }
+        }
+    }
+
+    /**
+     * 遍历目录寻找 sys.php 文件
+     * @param string $directory
+     */
+    protected function scanDirectoryForSysFiles(string $directory): void
+    {
         $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($baseDir),
+            new \RecursiveDirectoryIterator($directory),
             \RecursiveIteratorIterator::SELF_FIRST
         );
 
