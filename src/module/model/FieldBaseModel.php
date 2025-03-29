3ec23640e4423e3f5151dba774c60ad3099e688a
sunboy <qysk@vip.qq.com>
2025-03-29 11:20:27 +0800

    refactor(module/model): 重构 FieldBaseModel 中的 JSON 字段处理逻辑
    - 移除固定的 $json 和 $jsonAssoc 属性- 引入 getOptions 方法动态设置 JSON 字段类型和关联数组返回方式
    - 提高了代码的灵活性和可维护性

--- src/module/model/FieldBaseModel.php
+++ src/module/model/FieldBaseModel.php
@@ -7,6 +7,14 @@ use mowzs\lib\Model;
 
 abstract class FieldBaseModel extends Model
 {
-    protected $json = ['extend'];
-    protected $jsonAssoc = true;
+    protected function getOptions(): array
+    {
+        return [
+            'type' => [
+                // 设置JSON字段的类型
+                'extend' => 'json'
+            ],
+            'jsonAssoc' => true,  // 设置JSON数据返回数组
+        ];
+    }
 }
