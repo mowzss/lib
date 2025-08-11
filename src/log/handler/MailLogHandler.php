c9c9f9f8ebae97517f7336e1d53b66a558767fcf
sunboy <qysk@vip.qq.com>
2025-08-11 17:46:42 +0800

    feat(log): 增加邮件日志处理器
    - 在 config/log.php 中添加了 email 日志通道配置
    - 新增 MailLogHandler 类实现自定义日志处理器- 使用队列将日志信息异步发送到指定邮箱

---
+++ src/log/handler/MailLogHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace mowzs\lib\log\handler;
+
+use think\contract\LogHandlerInterface;
+use think\facade\Queue;
+
+abstract class MailLogHandler implements LogHandlerInterface
+{
+    protected $to;
+    protected $subject;
+
+    public function __construct($to, $subject = '系统日志')
+    {
+        $this->to = $to;
+        $this->subject = $subject;
+    }
+
+    /**
+     * 日志写入接口
+     * @access public
+     * @param array $log 日志信息
+     * @return bool
+     */
+    public function save(array $log): bool
+    {
+        // 将日志消息转换为字符串
+        $body = is_array($log) ? json_encode($log, JSON_UNESCAPED_UNICODE) : (string)$log;
+        try {
+            Queue::push(\app\job\system\SendEmailJob::class, ['to' => $this->to, 'subject' => $this->subject, 'body' => $body, 'isHtml' => true]);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+}
