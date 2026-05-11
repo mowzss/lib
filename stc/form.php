e894d226e75041378094ea3bed8dbcdcebaa5ddf
sunboy <qysk@vip.qq.com>
2026-05-11 22:02:49 +0800

    feat(config): 更新配置文件结构并添加表单构建字段支持
    - 从 happy-cms-core 中移除 form 配置项
    - 在 lib 模块中添加 form 配置支持
    - 新增 stc/form.php 配置文件，定义表单构建字段类型
    - 支持多种表单字段：输入框、选择框、上传组件等
    - 包含日期时间、编辑器、图标等多种组件类型配置

---
+++ stc/form.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 表单构建字段支持
+ */
+return [
+    'text' => '单行输入框',
+    'textarea' => '多行输入框',
+    'disabled' => '只读框',
+    'radio' => '单选框',
+    'checkbox' => '复选框',
+    'select' => '单选下拉框',
+    'xmselect' => '异步[单选/多选]',
+    'image' => '单图上传',
+    'images' => '多图上传',
+    'file' => '单文件上传',
+    'files' => '多文件上传',
+    'date' => '日期',
+    'datetime' => '日期+时间',
+    'daterange' => '日期区间',
+    'icon' => 'Icon图标',
+    'cron' => 'Cron表达式',
+    'color' => '颜色选择器',
+    'editor' => '富文本编辑器(默认)',
+    'tinymce' => 'tinymce编辑器',
+    'wangeditor' => 'WangEditor',
+    'ueditor' => '百度Ueditor',
+];
